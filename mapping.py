83e8043c3ca6cc96de6c73c04d04296dc0ed929f
Filipe Ramos <filipe.ramos@etu.unige.ch>
2024-04-14 15:35:39 +0200

    output format compressé

--- mapping.py
+++ mapping.py
@@ -16,7 +16,7 @@ df_ratings = read_tsv('title.ratings.tsv.gz')
 df_metrage = df_basics[df_basics['titleType'].isin(['movie', 'tvSeries'])]
 df_metrage = df_metrage[['tconst', 'primaryTitle', 'startYear', 'genres']]
 df_metrage.columns = ['ID_Metrage', 'Titre', 'Annee', 'Categorie']
-df_metrage.to_csv('metrage.csv', index=False)
+df_metrage.to_csv('metrage.csv.gz', index=False, compression='gzip')
 
 # Tables Film et Serie avec note moyenne
 df_film_series = pd.merge(df_basics[['tconst', 'titleType']], df_crew[['tconst', 'directors']], on='tconst')
@@ -25,29 +25,29 @@ df_film_series = pd.merge(df_film_series, df_ratings[['tconst', 'averageRating']
 df_film = df_film_series[df_film_series['titleType'] == 'movie']
 df_film = df_film[['tconst', 'directors', 'averageRating']]
 df_film.columns = ['ID_Film', 'Realisateur', 'note_moyenne']
-df_film.to_csv('film.csv', index=False)
+df_film.to_csv('film.csv.gz', index=False, compression='gzip')
 
 df_serie = df_film_series[df_film_series['titleType'] == 'tvSeries']
 df_serie = df_serie[['tconst', 'directors', 'averageRating']]
 df_serie.columns = ['ID_Serie', 'Realisateur', 'note_moyenne']
-df_serie.to_csv('serie.csv', index=False)
+df_serie.to_csv('serie.csv.gz', index=False, compression='gzip')
 
 # Table Episode
 df_episode_export = pd.merge(df_episode, df_basics[['tconst', 'primaryTitle']], on='tconst')
 df_episode_export = df_episode_export[['tconst', 'parentTconst', 'seasonNumber', 'episodeNumber', 'primaryTitle']]
 df_episode_export.columns = ['ID_Episode', 'ID_Serie', 'NumeroSaison', 'NumeroEpisode', 'TitreEpisode']
-df_episode_export.to_csv('episode.csv', index=False)
+df_episode_export.to_csv('episode.csv.gz', index=False, compression='gzip')
 
 # Table Personne
 df_people_export = df_people[['nconst', 'primaryName', 'birthYear', 'deathYear']]
 df_people_export.columns = ['ID_Personne', 'Nom', 'DateNaissance', 'DateMort']
-df_people_export.to_csv('personne.csv', index=False)
+df_people_export.to_csv('personne.csv.gz', index=False, compression='gzip')
 
 # Préparation des tables Distribution et Distribution_Personne
 df_distribution = df_principals[['tconst', 'nconst', 'category']].drop_duplicates()
 df_distribution['ID_Distribution'] = pd.factorize(df_distribution['tconst'])[0] + 1  # Création d'un ID unique simplifié
-df_distribution.to_csv('distribution.csv', index=False)
+df_distribution.to_csv('distribution.csv.gz', index=False, compression='gzip')
 
 df_distribution_personne = df_distribution[['ID_Distribution', 'nconst', 'category']]
 df_distribution_personne.columns = ['ID_Distribution', 'ID_personne', 'Role']
-df_distribution_personne.to_csv('distribution_personne.csv', index=False)
+df_distribution_personne.to_csv('distribution_personne.csv.gz', index=False, compression='gzip')
