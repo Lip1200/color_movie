d42f36aad593418b63704dcecc3fd9f551b58986
Filipe Ramos <2978-Filipe.Ramos@users.noreply.gitlab.unige.ch>
2024-05-08 13:34:37 +0200

    clean venv

--- src/models/local/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base import Base, metrage_genre_association, db
-from .critique import Critique
-from .credit import Credit
-from .liste import Liste
-from .metrage import Metrage
-from .personne import Personne
-from .utilisateur import Utilisateur
-from .entree_liste import EntreeListe
-from .genre import Genre
