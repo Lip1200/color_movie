795919cdaf354641046b1d920a32cc46f9889163
Filipe Ramos <filipe.ramos@etu.unige.ch>
2024-05-21 00:18:45 +0200

    ajouts des notes users pour leurs listes de films favorits

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 class Config:
     load_dotenv()
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://' + os.environ.get("MOVIE_DB_USER", "") + ':' + os.environ.get("MOVIE_DB_PASSWORD","") + '@localhost/MovieDb'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://movienotes:filipe@localhost/MovieDb'
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + os.environ.get("MOVIE_DB_USER", "") + ':' + os.environ.get("MOVIE_DB_PASSWORD","") + '@localhost/MovieDb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("FLASK_SK", "")
