fdfc981927ef25894328de2a8427cb2514145a38
Filipe Ramos <filipe.ramos@etu.unige.ch>
2024-05-21 02:15:11 +0200

    compose pour chromadb, début d'essais de vectorisation des films.
    Former-commit-id: c3236fd1100dea59070fbc47dbc5850df3804d49

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 class Config:
     load_dotenv()
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://movienotes:REDACTED@localhost/MovieDb'
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + os.environ.get("MOVIE_DB_USER", "") + ':' + os.environ.get("MOVIE_DB_PASSWORD","") + '@localhost/MovieDb'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + os.environ.get("MOVIE_DB_USER", "movienotes") + ':' + os.environ.get("MOVIE_DB_PASSWORD","filipe") + '@localhost/MovieDb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("FLASK_SK", "")
