b7d94501a720da7b9845c20f28b97ce65048599b
Filipe Ramos <filipe.ramos@etu.unige.ch>
2024-06-15 21:58:54 +0200

    modif de next pour voir si ça fonctionne
    Former-commit-id: 5f6d8504cdc0461decd6a3c6299d683822d19020
    Former-commit-id: b291e73665b45489d8f7b1b9f83cd5008b623e82

--- interface/next.config.mjs
+++ interface/next.config.mjs
@@ -22,22 +22,6 @@ export default {
             key: 'Surrogate-Control',
             value: 'no-store',
           },
-          {
-            key: 'Access-Control-Allow-Credentials',
-            value: 'true',
-          },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'http://nextjs:3000 http://selenium-hub:4444',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
         ],
       },
     ];
